774bd7d567573e01e7f5a39226769a5b4e028b90
Darren Taggart <dazzletag@yahoo.co.uk>
2026-01-26 11:14:44 +0000

    fixed deploy

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -3,7 +3,7 @@ param(
   [string]$sqlPassword = $env:SQL_ADMIN_PASSWORD,
   [string]$aadTenantId = $env:AAD_TENANT_ID,
   [string]$webClientId = $env:AAD_CLIENT_ID,
-  [string]$apiAudience = $env:API_AUDIENCE ?? "api://sundries-api"
+  [string]$apiAudience = $env:API_AUDIENCE
 )
 
 Set-StrictMode -Version Latest
@@ -20,6 +20,10 @@ if (-not $webClientId) {
   throw "Set AAD_CLIENT_ID (web client) before running this script."
 }
 
+if (-not $apiAudience) {
+  $apiAudience = "api://sundries-api"
+}
+
 Write-Host "Ensuring account is initialized..."
 az account show > $null
 
@@ -27,7 +31,7 @@ $principalId = $null
 if ($env:AZURE_CLIENT_ID) {
   $principalId = az ad sp show --id $env:AZURE_CLIENT_ID --query objectId -o tsv
 } else {
-  $principalId = az ad signed-in-user show --query objectId -o tsv
+  $principalId = az ad signed-in-user show --query id -o tsv
 }
 
 if (-not $principalId) {
@@ -46,10 +50,27 @@ $parameters = @{
 }
 
 Write-Host "Deploying infrastructure..."
-az deployment group create \
-  --resource-group BCHSystems \
-  --template-file "$PSScriptRoot\main.bicep" \
-  --parameters $parameters
+$parameterFile = Join-Path $env:TEMP ("sundries-deploy-parameters-{0}.json" -f [guid]::NewGuid())
+$parameterTemplate = @{
+  '$schema' = 'https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#'
+  contentVersion = '1.0.0.0'
+  parameters = @{}
+}
+
+foreach ($key in $parameters.Keys) {
+  $parameterTemplate.parameters[$key] = @{
+    value = $parameters[$key]
+  }
+}
+
+$parameterTemplate | ConvertTo-Json -Depth 10 | Set-Content -Path $parameterFile -Encoding utf8
+
+try {
+  az deployment group create --resource-group BCHSystems --template-file "$PSScriptRoot\main.bicep" --parameters "@$parameterFile"
+}
+finally {
+  Remove-Item -Path $parameterFile -ErrorAction SilentlyContinue
+}
 
 
 
