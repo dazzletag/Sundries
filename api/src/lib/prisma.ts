d83744ccb44938f17cfe37bf15458020136204ce
Darren Taggart <dazzletag@yahoo.co.uk>
2026-01-27 09:27:53 +0000

    prisma.ts fix

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  adapter: {
-    url: process.env.DATABASE_URL!,
-  },
-  log: process.env.NODE_ENV === "development" ? ["query", "info"] : ["error"],
-});
+export const prisma = new PrismaClient();
+
+
